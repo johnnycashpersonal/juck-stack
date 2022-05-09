304ba491c7010a8e6b8e9412f7dcfd12517b3c46
Michal Young <michal.young@gmail.com>
2022-05-08 21:30:06 -0700

    Repaired damage from a global replace

--- programs/asm/fact.asm
+++ programs/asm/fact.asm
@@ -1,27 +1,28 @@
-# Lovingly crafted by robots
-# 2018-05-24 10:13:01.543036 from programs/fact.awl
+# Lovingly crafted by the robots of CIS 211
+# 2022-05-08 21:18:10.596602 from programs/mal/fact.mal
 #
-	LOAD r1,r0,r0[510]
-	STORE  r1,x_1
-	LOAD r1,const1_3  # Const 1
-	STORE  r1,fact_2
-loop_4:  #While loop
-	LOAD r2,x_1
-	SUB  r0,r2,r0 
-	JUMP/Z endloop_5
-	LOAD r1,fact_2
-	LOAD r2,x_1
-	MUL  r1,r1,r2
-	STORE  r1,fact_2
-	LOAD r1,x_1
-	LOAD r2,const1_3  # Const 1
-	SUB  r1,r1,r2
-	STORE  r1,x_1
-	JUMP loop_4
-endloop_5: 
-	LOAD r1,fact_2
-	STORE  r1,r0,r0[511]
+   LOAD  r14,r0,r0[510]
+   STORE  r14,var_x
+    LOAD r14,const_1
+   STORE  r14,var_fact
+while_do_1:
+    LOAD r14,var_x
+    LOAD r13,const_1
+   SUB  r0,r14,r13
+   JUMP/ZM  od_2  #>
+    LOAD r14,var_fact
+    LOAD r13,var_x
+   MUL  r14,r14,r13
+   STORE  r14,var_fact
+    LOAD r14,var_x
+    LOAD r13,const_1
+   SUB  r14,r14,r13
+   STORE  r14,var_x
+   JUMP  while_do_1
+od_2:
+    LOAD r14,var_fact
+   STORE  r14,r0,r0[511]
 	HALT  r0,r0,r0
-x_1: DATA 0 #x
-fact_2: DATA 0 #fact
-const1_3:  DATA 1
+const_1:  DATA 1
+var_x:  DATA 0
+var_fact:  DATA 0
--- programs/asm/max.asm
+++ programs/asm/max.asm
@@ -1,14 +1,21 @@
-# Determine the maximum of two integers.
-# This program triggers memory-mapped IO to
-# read integers from the keyboard and to
-# write integers to the display.
-   LOAD r1,r0,r0[510]     # Trigger read from console
-   LOAD r2,r0,r0[510]     # Trigger read from console
-   SUB  r0,r1,r2[0]
-   JUMP/P r1bigger
-   STORE r2,r0,r0[511]    # Trigger write to console
-   HALT r0,r0,r0
-r1bigger:
-   STORE r1,r0,r0[511]    # Trigger write to console
-   HALT r0,r0,r0
-   
+# Lovingly crafted by the robots of CIS 211
+# 2022-05-08 21:18:10.679691 from programs/mal/max.mal
+#
+   LOAD  r14,r0,r0[510]
+   STORE  r14,var_x
+   LOAD  r14,r0,r0[510]
+   STORE  r14,var_y
+    LOAD r14,var_x
+    LOAD r13,var_y
+   SUB  r0,r14,r13
+   JUMP/ZM  else_1  #>
+    LOAD r14,var_x
+   STORE  r14,r0,r0[511]
+	JUMP fi_2
+else_1:
+    LOAD r14,var_y
+   STORE  r14,r0,r0[511]
+fi_2:
+	HALT  r0,r0,r0
+var_x:  DATA 0
+var_y:  DATA 0
